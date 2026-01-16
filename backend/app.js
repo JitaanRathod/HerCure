419bd3cf57c3a4ab5373c8c25458e29bc1a558c5
KahanMody15 <modykahan@gmail.com>
2026-01-16 22:30:20 +0530

    Sprint 2A: JWT authentication APIs implemented

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-
+const authRoutes = require("./routes/auth.routes");
 const app = express();
+const authMiddleware = require("./middleware/auth.middleware");
+
+app.get("/api/test/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
+});
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,76 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+/**
+ * POST /api/auth/register
+ */
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // basic validation
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    return res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Register error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+/**
+ * POST /api/auth/login
+ */
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password required" });
+    }
+
+    // find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // generate token
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.error("Login error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
